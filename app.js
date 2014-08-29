31bf152e104bb2b3d4f7e44fc23d0ebf34e6d42e
Ele Palmer <ecpalmer21@gmail.com>
2014-08-29 15:43:54 -0400

    adds item
    submit button was submitting form, changed type to button. Now button
    adds one extra div but will not add multiple or value of input

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var $value = $("#additemval").val();
+    var $newitem = $("<div class='item'><h1></h1>" + $value + "<div class='btn'>Done!</div><div class='dlt'>Delete</div></div>");
+    
+    
     $("#additem")
-      .on("click", function() {
-    var value = $("#additemval").val();
-    var newitem = $("<div>", {class: "item"});
+      .click(function () {
     //on button click, add item variable
-    $("#list").append(newitem);
-    newitem.append(value).append("</div>");
-    //newitem.addClass("item");
+    $("#list").append($newitem); 
+    //$newitem.append($value);
+    
       });
     
     //on done click, strikethough item
     $(".btn")
-        .on("click", function() {      $(this).prev().toggleClass("done"); 
+        .on("click", function () {      $(this).prev().toggleClass("done"); 
         });
     
    //on delete click, remove item
     $(".dlt")
-        .on("click", function() {
+        .on("click", function () {
         $(this).parent().remove();  
         });
 
